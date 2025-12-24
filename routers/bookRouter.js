d4dcc250f0005a738aada341673637eee8d401dd
Muskan yadav <muskanyadav92163@gmail.com>
2025-12-24 21:27:22 +0530

    Add image upload middleware to createBook route

--- routers/bookRouter.js
+++ routers/bookRouter.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/controller");
 const router = express.Router();
 
 router.get("/", controller.showAllBooks);
-router.post("/createBook", controller.createBook);
+router.post("/createBook", upload.single("image"), controller.createBook);
 router.get("/edit/:id", controller.editBookForm);
 router.get("/delete/:id", controller.deleteBook)
 
