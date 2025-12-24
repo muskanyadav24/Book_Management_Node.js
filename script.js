1a43a76425f4aff5845f22e3565996d395835377
Muskan yadav <muskanyadav92163@gmail.com>
2025-12-24 21:31:04 +0530

    Update server port and improve formatting

--- script.js
+++ script.js
@@ -2,27 +2,25 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const connectDB = require("./db/bookDB");
+const upload = require("./middlewaes/middle");
 
 const server = express();
 
 server.set('view engine', 'ejs');
-server.use(bodyParser.urlencoded({extended:true}));
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(express.static("assets"));
+server.use("/uploads", express.static("uploads"));
 
 connectDB();
-
-// server.get('/', (req, res) => {
-//     res.render('index');
-// });
 
 const bookRouter = require("./routers/bookRouter")
 server.use("/", bookRouter);
 
-server.listen(3000, (err) => {
-    if(!err){
-        console.log("Server is running on http://localhost:3000");
+server.listen(7000, (err) => {
+    if (!err) {
+        console.log("Server is running on http://localhost:7000");
         console.log("Server is successfully started");
-    }else{
+    } else {
         console.log("Error...", err);
     }
 });
